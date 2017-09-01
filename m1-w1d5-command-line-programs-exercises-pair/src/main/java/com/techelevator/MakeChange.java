490c40068fd92ff14e2e96b530f2b5842ab6dd0a
Jared Malvic <jarediscool92@gmail.com>
2017-09-01 13:59:59 -0400

    Completed both programs WITHOUT splitting arrays

--- m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MakeChange.java
+++ m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MakeChange.java
@@ -1,17 +1,40 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then display the change required.
  
  $ java MakeChange
- Please enter the amount of the bill: 23.65
- Please enter the amount tendered: 100.00
+ Please enter the amount of the bill: 23.65 billAmount
+ Please enter the amount tendered: 100.00 payAmount
  The change required is 76.35
  */
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
+		System.out.print("Please enter the amount of the bill: ");
+		
+		double billAmount = input.nextDouble();
+		input.nextLine();
+		
+		System.out.print("PLease enter the amount paid: ");
+		
+		double payAmount = input.nextDouble();
+		input.nextLine();
+		
+		System.out.println( "Your change is " + makeChange(billAmount, payAmount));
 	}
 
+	public static double makeChange (double billAmount, double payAmount) {
+	 
+		double change;				
+		change = payAmount - billAmount; 
+			return change;
+				
+	}
+	
+	
 }
